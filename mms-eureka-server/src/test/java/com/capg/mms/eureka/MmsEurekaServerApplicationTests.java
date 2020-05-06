7186317e06b7e9f1ce12366d443437ab72f95d51
hp <hp@192.168.0.5>
2020-05-06 21:00:28 +0530

    commit by rohith

---
+++ mms-eureka-server/src/test/java/com/capg/mms/eureka/MmsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.mms.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
